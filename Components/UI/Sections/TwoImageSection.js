a5d7768896f5db99ddba379409646a9368a548ac
Gurpreet <dhoat30@gmail.com>
2023-01-12 18:28:56 +1300

    removing grid from image section

--- Components/UI/Sections/TwoImageSection.js
+++ Components/UI/Sections/TwoImageSection.js
@@ -5,6 +5,7 @@ import ColumnTitle from '../Typography/ColumnTitle'
 import Paragraph from '../Typography/Paragraph'
 import PrimaryButton from '../Buttons/PrimaryButton'
 import useMediaQuery from '@mui/material/useMediaQuery';
+import { Paper } from '@mui/material';
 
 function TwoImageSection({ title, content, backgroundImage, frontImage, imageText, callToActionText, callToActionLink, hideImageOnMobile }) {
     const matches = useMediaQuery('(min-width:900px)');
@@ -14,23 +15,28 @@ function TwoImageSection({ title, content, backgroundImage, frontImage, imageTex
             {matches ?
                 <Container style={{ background: "var( --sanger--theme--white) " }}>
                     <TwoColumns className='grid max-width'>
-                        <div className='first-image-container'>
+                        {/* <div className='first-image-container'>
                             <Image fill={true} src={backgroundImage.url} alt={backgroundImage.alt ? backgroundImage.alt : "Background Image"} sizes="50vw" />
                         </div>
-                        <Overlay></Overlay>
-                        <div className='image-text'>
-                            <h6 className="display-large">{imageText} </h6>
-                            <p className="label-medium"> Years of Experience</p>
-                            <svg width="55" height="54" viewBox="0 0 55 54" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M23.5537 17.3536L37.2505 17.3536L37.2505 30.9454M18.0705 36.3866L37.0587 17.544" stroke="#241A00" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                        </div>
+                        <Overlay></Overlay> */}
+
                         <div className="second-image-container">
-                            <Image src={frontImage.url} fill={true} alt={frontImage.alt ? frontImage.alt : "front image"} sizes="50vw" />
+                            <Image src={frontImage.url} width="100" height="100" alt={frontImage.alt ? frontImage.alt : "front image"} sizes="50vw" />
                         </div>
+                        <Paper elevation={2} className='image-text'>
+
+                            <div >
+                                <h6 className="display-large">{imageText} </h6>
+                                <p className="headline-medium"> Years of Experience</p>
+                                <svg width="55" height="54" viewBox="0 0 55 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M23.5537 17.3536L37.2505 17.3536L37.2505 30.9454M18.0705 36.3866L37.0587 17.544" stroke="#241A00" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg>
+                            </div>
+                        </Paper>
                         <ContentColumn>
                             <ColumnTitle align="left"> {title} </ColumnTitle>
                             <Paragraph >{content}</Paragraph>
+
                             <PrimaryButton variant="contained" callToActionText={callToActionText} href={callToActionLink} />
                         </ContentColumn>
                     </TwoColumns>
@@ -65,45 +71,39 @@ export default TwoImageSection
 const Container = styled.section`
 display: flex ;
 align-items: flex-end; 
-margin-top: 240px; 
-margin-bottom: -120px; 
+margin-top: 120px; 
+margin-bottom: -80px; 
 `
 const TwoColumns = styled.div`
 position: relative ;
-top: -120px; 
-.first-image-container{ 
-    grid-column: 1/6;
-    grid-row: 1/8;
-}
-.second-image-container{ 
-    grid-column: 3/8;
-    grid-row: 5/12;
+top: -80px; 
+
+.second-image-container{  
+    grid-column: 1/7;
+    grid-row: 1/10;
     img { 
         filter: saturate(130%);
-    }
-}
- >div{ 
-    position: absolute;
-    width: 100%; 
-    height: 100%;
-    img{ 
+        width: 100%; 
+        height: 100%;
         object-fit: cover; 
         object-position: 100% 100%;
     }
 }
+
 .image-text{ 
     background: var(--sanger--theme--sys--light--secondary-container);
-    grid-column: 2/4;
-    grid-row: 4/6;
+    
+    grid-column: 5/7;
+    grid-row: 7/10;
     position: relative;
-    left: 60px; 
-    top: 10px;
-    display: flex; 
+     left: 60px; 
+   top: 60px;
+
     padding: 20px 20px; 
- 
-    width: 250px; 
+    border-radius: 4px; 
+    display:flex; 
+    align-items: center; 
     p{ 
-        margin: 5px 0 10px 20px; 
     }
     svg{ 
         position: absolute ;
@@ -119,8 +119,11 @@ const Overlay = styled.div`
 `
 
 const ContentColumn = styled.div`
-        grid-column: 9/13;
-   grid-row: 6/13;
+position: sticky ;
+top: 10;
+        /* grid-column: 9/13; */
+   /* grid-row: 3/13; */
+   width: 100%; 
    a{ 
     margin-top: 16px; 
     display: inline-block ;
@@ -132,6 +135,8 @@ const ContentColumn = styled.div`
     margin-top: 16px; 
    }
 `
+
+// mobile css 
 const MobileContainer = styled.section`
 margin-top: 80px; 
 
